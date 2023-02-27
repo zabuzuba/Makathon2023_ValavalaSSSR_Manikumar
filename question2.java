fbd5ad684777591aa67d506cfdc0e53a89f3cce4
zabuzuba <99137355+zabuzuba@users.noreply.github.com>
2023-02-27 15:01:05 +0530

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the T value");
+        int t = sc.nextInt();
+        int i;
+        int[] in = new int[t];
+        System.out.println("Enter the n values");
+        for (i = 0; i < in.length; i++) {
+            in[i] = sc.nextInt();
+        }
+        int[] out = new int[in.length];
+        for (i = 0; i < out.length; i++) {
+            if (in[i] % 2 == 0) {
+
+            }
+        }
+    }
+}
